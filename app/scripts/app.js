5f7c5ef53ec38b9d76ecdd9b23d85d3ed6bab842
tauhir <mtauhire@gmail.com>
2024-04-23 13:40:47 +0200

    updated app.js to use html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,21 +7,36 @@ async function init() {
     client.events.on('app.activated', renderCustomerInfo);
 }
 
+// async function renderCustomerInfo() {
+//     const textElement = document.getElementById('apptext');
+//     const contactData = await client.data.get('contact');
+//     const {
+//       contact: { name }
+//     } = contactData;
+//     console.info("start");
+//     console.log(contactData)
+//     textElement.innerHTML = `Ticket is created by ${name}`;
+// }
+
 async function renderCustomerInfo() {
     const textElement = document.getElementById('apptext');
     const contactData = await client.data.get('contact');
 
     if (contactData && contactData.contact && contactData.contact.email) {
+
+        console.log("Contact Data incoming:")
+        console.info(contactData);
         const email = contactData.contact.email;
         // Replace 'https://yourbackend.com/customer-info' with your actual backend endpoint
         // and 'YOUR_API_TOKEN' with your actual API token
-        const response = await fetch(`https://yourbackend.com/customer-info?email=${email}`, {
+        console.log(email);
+
+        const response = await fetch(`https://5e3ed6b5-db01-4262-8252-d36563dcf3c7.mock.pstmn.io/customer-info?email=${email}`, {
             method: 'GET',
             headers: {
                 'Authorization': 'Bearer API_TOKEN',
             },
         });
-
         if (response.ok) {
             // JSON should contain a list of users with their client group ids, status, and number of xero connections
             //It can also contain the HTML that we've previously used.
@@ -50,7 +65,15 @@ async function renderCustomerInfo() {
 
             // as a V1, just update the html with what was given
             const data = await response.json();
-            textElement.innerHTML = data.html;
+            console.info('succesfully got data from SP');
+            console.info(data);
+            if (data.html) {
+                textElement.innerHTML = data.html;
+            } else {
+                textElement.innerText = "No HTML content available";
+            }
+            //textElement.innerHTML = data.html;
+
         } else {
             textElement.innerText = "Failed to fetch customer info";
         }
